578b385a18ab576816d83beeedbe2edff5e1dada
shadowmoonezios <agushifelde@hotmail.com>
2025-10-05 20:46:48 +0300

    feat: initial commit for client/src/App.js

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Gemeinschaftskalender</h1>
+      <p>Willkommen im Gemeinschaftskalender!</p>
+    </div>
+  );
+}
+
+export default App;
